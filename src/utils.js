eec33c938751193d2136805586372ee4ab3cd335
samiulalimsaad <saadnbiuraj@gmail.com>
2022-09-01 00:30:05 +0600

    updated `base url`

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://fake-todo-server.herokuapp.com";
+export const BASE_URL = "https://fake-api-json-server-saad.herokuapp.com/todos";
 // export const BASE_URL = "http://localhost:4000";
